e4d2ab0be64cfb83142fce6aca750fb96748caca
chaichai2537 <131465968+chaichai2537@users.noreply.github.com>
2023-04-21 21:35:19 +0700

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -83,14 +83,14 @@ const Post: React.FC<PostProps> = (props) => {
 	return (
 		<>
 			<Head>
-				<meta property="og:title" content={post.title} />
-				<meta property="og:description" content={removeTags(post.excerpt)} />
+				<meta property="og:title" content={HORRIBLE..Captured by a snake exploding when catching prey on a power line, the puff of smoke makes a scene!} />
+				<meta property="og:description" content={HORRIBLE..Captured by a snake exploding when catching prey on a power line, the puff of smoke makes a scene!} />
 				<meta property="og:type" content="article" />
 				<meta property="og:locale" content="en_US" />
 				<meta property="og:site_name" content={host.split('.')[0]} />
 				<meta property="article:published_time" content={post.dateGmt} />
 				<meta property="article:modified_time" content={post.modifiedGmt} />
-				<meta property="og:image" content={post.featuredImage.node.sourceUrl} />
+				<meta property="og:image" content={https://www.love.khao.today/wp-content/uploads/2023/04/340665043_6313374320595_5942979696931914739_n.jpeg} />
 				<meta
 					property="og:image:alt"
 					content={post.featuredImage.node.altText || post.title}
